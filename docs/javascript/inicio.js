194d4c41725ea63762fbc58741a85c2ae32ee3cf
Eleiza Caridad <eleizacaridad@gmail.com>
2023-03-28 17:38:29 -0500

    ready

--- docs/javascript/inicio.js
+++ docs/javascript/inicio.js
@@ -1,16 +1,12 @@
-function loguear()
-{
-    let user=document.getElementById("usuario").value;
-    let pass=document.getElementById("clave").value;
-    let msg=document.getElementById("msg");
+function loguear() {
+  let user = document.getElementById("usuario").value;
+  let pass = document.getElementById("clave").value;
+  let msg = document.getElementById("msg");
 
-    if(user=="bit" && pass=="2023")
-    {
-        window.location="https://eleizac.github.io/bit06ejercicios/ejercicios.html";
-    }
-    else
-    {
-        alert("Datos incorrectos")
-
-    }
+  if (user == "bit" && pass == "2023") {
+    window.location =
+      "https://eleizac.github.io/bit06ejercicios/ejercicios.html";
+  } else {
+    alert("Datos incorrectos");
+  }
 }
